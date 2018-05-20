9dcbe88e742ab34470dfaa84c30cbe81e56c7fea
Lesya Volobueva <foxolesia.222@gmail.com>
2018-05-20 11:55:35 +0300

    changed server port

--- app/src/utils/callApi.js
+++ app/src/utils/callApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:3100';
+const API_URL = 'http://localhost:3001';
 
 export default function callApi(endpoint, method = 'get', body)  {
     return axios[method](`${API_URL}/${endpoint}`, body);
