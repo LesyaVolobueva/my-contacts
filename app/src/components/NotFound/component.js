3bda8be6312f3af507ffa41f4c9b7b195214954e
Olesia_Volobuieva <Olesia_Volobuieva@epam.com>
2018-06-01 16:07:54 +0300

    add NotFound page

---
+++ app/src/components/NotFound/component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='not-found'>
+      <h3 className='not-found__header'>Page not found</h3>
+    </div>
+  );
+};
+
+
+export default NotFound;
